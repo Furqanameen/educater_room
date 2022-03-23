621e6bd9b9c23c409a70202c26b4de79ef816f16
furqanbinameen <furqanbinameen@gmail.com>
2022-03-24 01:23:05 +0500

    change name

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
-admin  = User.create(first_name: 'Admin', last_name: 'BrightersRoom', email: 'admin@brightersroom.com', password: 'adm!n12E', role: :admin)
-author = User.create(first_name: 'Instructor', last_name: 'BrightersRoom', email: 'instructor@brightersroom.com', password: '!nstuct0r', role: :instructor)
+admin  = User.create(first_name: 'Admin', last_name: 'educatersRoom', email: 'admin@educatersroom.com', password: 'adm!n12E', role: :admin)
+author = User.create(first_name: 'Instructor', last_name: 'educatersRoom', email: 'instructor@educatersroom.com', password: '!nstuct0r', role: :instructor)
 
 puts "Admin Created!"  if admin.present?
 puts "Author Created!" if author.present?
